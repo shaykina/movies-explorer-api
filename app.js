4029b583000f003976e6699f636c8a8148c1e636
Anastasiia Shaikina <shaikina@phystech.edu>
2023-07-25 16:56:33 +0200

    fix: поправить ошибки cors

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(cors({
     'https://localhost:3000',
     'http://localhost:3000',
   ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
 }));
 
 app.use(requestLogger);
