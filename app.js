2d00ab0f89ecdbc3a46025a0e48a3551c501b7eb
Anastasiia Shaikina <shaikina@phystech.edu>
2023-07-25 15:38:31 +0200

    fix: поправить доменное имя для фронтенда

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const routerUsers = require('./routes/users');
+const routerMovies = require('./routes/movies');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/NotFoundError');
+require('dotenv').config();
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { DATABASE_URL, NODE_ENV } = process.env;
+
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb', {});
+
+app.use(cors({
+  origin: [
+    'https://movies-shaykina.nomoredomains.xyz',
+    'http://movies-shaykina.nomoredomains.xyz',
+    'https://localhost:3000',
+    'http://localhost:3000',
+  ],
+}));
+
+app.use(requestLogger);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.use(auth);
+
+app.use('/users', routerUsers);
+app.use('/movies', routerMovies);
+app.use('/', (req, res, next) => {
+  next(new NotFoundError('Указанный путь не найден'));
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
